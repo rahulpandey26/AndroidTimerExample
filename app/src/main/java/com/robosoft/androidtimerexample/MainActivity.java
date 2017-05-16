75c889167cd628ba0dd11a962ede22fff35cc367
rahul <rahul.pandey@robosoftin.com>
2017-05-16 11:33:47 +0530

    optimized code.

--- app/src/main/java/com/robosoft/androidtimerexample/MainActivity.java
+++ app/src/main/java/com/robosoft/androidtimerexample/MainActivity.java
@@ -5,56 +5,62 @@ import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private TextView timerValue;
-    private long startTime = 0L;
-    private Handler customHandler = new Handler();
-    private long timeInMilliseconds = 0L;
-    private long timeSwapBuff = 0L;
-    private long updatedTime = 0L;
+    private TextView mTimerValue;
+    private long mStartTime = 0L;
+    private Handler handler = new Handler();
+    private long mTimeInMilliseconds = 0L;
+    private long mTimeSwapBuff = 0L;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // initialize view
+        initView();
+    }
 
-        timerValue = (TextView) findViewById(R.id.timerValue);
+    private void initView() {
+        mTimerValue = (TextView) findViewById(R.id.timerValue);
+        findViewById(R.id.startButton).setOnClickListener(this);
+        findViewById(R.id.pauseButton).setOnClickListener(this);
+    }
 
-        findViewById(R.id.startButton).setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.startButton:
+                mStartTime = SystemClock.uptimeMillis();
+                handler.postDelayed(updateTimerThread, 0);
+                break;
 
-            public void onClick(View view) {
-                startTime = SystemClock.uptimeMillis();
-                customHandler.postDelayed(updateTimerThread, 0);
-            }
-        });
+            case R.id.pauseButton:
+                mTimeSwapBuff += mTimeInMilliseconds;
+                handler.removeCallbacks(updateTimerThread);
+                break;
 
-        findViewById(R.id.pauseButton).setOnClickListener(new View.OnClickListener() {
+            default: // do nothing
 
-            public void onClick(View view) {
-                timeSwapBuff += timeInMilliseconds;
-                customHandler.removeCallbacks(updateTimerThread);
-            }
-        });
+        }
     }
 
     private Runnable updateTimerThread = new Runnable() {
 
         public void run() {
-            timeInMilliseconds = SystemClock.uptimeMillis() - startTime;
-            updatedTime = timeSwapBuff + timeInMilliseconds;
+            mTimeInMilliseconds = SystemClock.uptimeMillis() - mStartTime;
+            long updatedTime = mTimeSwapBuff + mTimeInMilliseconds;
 
             int secs = (int) (updatedTime / 1000);
             int mins = secs / 60;
             secs = secs % 60;
             int milliseconds = (int) (updatedTime % 1000);
-            timerValue.setText("" + mins + ":"
+            mTimerValue.setText("" + mins + ":"
                     + String.format("%02d", secs) + ":"
                     + String.format("%03d", milliseconds));
-            customHandler.postDelayed(this, 0);
+            handler.postDelayed(this, 0);
         }
     };
 }
